ce9f4f4a6a2c6c7849e5e6ea1d5caabddbdaa9a9
Cadu <cadu666dsantos@gmail.com>
2022-07-29 09:37:38 -0300

    first commit

---
+++ src/ZapFood.WinForm/Model/PedidoMeioMeioComplementos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZapFood.WinForm.Model
+{
+    public class PedidoMeioMeioComplementos
+    {
+        public int PedidoMeioMeioComplementosID { get; set; }
+        public string NomeDaMetade { get; set; }
+        public string Nome { get; set; }
+        public decimal Valor { get; set; }
+        public int Quantidade { get; set; }
+        public Guid PedidoGuid { get; set; }
+
+    }
+}
---
+++ src/ZapFood.WinForm/Model/RootModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace ZapFood.WinForm.Model
+{
+    public class RootModel
+    {
+        public RootModel()
+        {
+            results = new List<PedidoMeioMeioComplementos>();
+        }
+        public int totalPage { get; set; }
+        public List<PedidoMeioMeioComplementos> results { get; set; }
+    }
+
+    public class teste
+    {
+        public string Complementos { get; set; }
+    }
+}
